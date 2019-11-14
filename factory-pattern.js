d8c56af95c275e58bd10ecfb847522fb9d8b4526
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-11-14 13:40:28 -0300

    factory-pattern

---
+++ factory-pattern.js
@@ -0,0 +1,49 @@
+function Developer(name) {
+   this.name = name
+   this.type = "Developer"
+}
+
+function Tester(name) {
+   this.name = name
+   this.type = "Tester"
+}
+
+function Costumer(name) {
+   this.name = name
+   this.type = "Costumer"
+}
+
+function EmployeeFactory() {
+  this.create = function(name, type) {
+    switch(type) {
+       case 1:
+        return new Developer(name)
+        break;
+      case 2:
+        return new Tester(name)
+        break;
+      case 3:
+        return new Costumer(name)
+        break;
+    }
+  }
+}
+
+function details(name, type) {
+  console.log("Hi, I am " + name + " and I am a " + type)
+  // Called in line 48
+  // console.log("Hi, I am " + this.name + " and I am a " + this.type)
+}
+
+const employees = [];
+const employeeFactory = new EmployeeFactory();
+
+employees.push(employeeFactory.create("Patrick", 1));
+employees.push(employeeFactory.create("John", 2));
+employees.push(employeeFactory.create("Tim", 2));
+employees.push(employeeFactory.create("David", 3));
+
+employees.forEach(function (emp) {
+  details(emp.name, emp.type)
+  // details.call(emp)
+});
